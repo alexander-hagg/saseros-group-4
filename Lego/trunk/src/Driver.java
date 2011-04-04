d0855af072dafe69eb05dfa51916420210e21a7d
akuryou.hero@gmail.com <akuryou.hero@gmail.com@57907b63-806d-ccc0-284c-5e2be84220fe>
2011-04-04 12:29:52 +0000

    Test

---
+++ Lego/trunk/src/Driver.java
@@ -0,0 +1,12 @@
+import lejos.nxt.*;
+
+public class Driver
+{
+    public Driver() {}
+
+    public static void main( String[] args )
+    {
+        Motor.A.forward();
+        Motor.B.forward();
+    }
+}
